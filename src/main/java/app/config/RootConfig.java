81b38350a8ab271c6894f66ce728789d5464ad09
ngnmhieu <ngnmhieu@gmail.com>
2016-02-24 14:45:36 +0100

    annotated configuration

--- src/main/java/app/config/RootConfig.java
+++ src/main/java/app/config/RootConfig.java
@@ -1,8 +1,17 @@
 package app.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 /**
  * @author ngnmhieu
  */
+@Configuration
+@ComponentScan(basePackages = "app",
+        excludeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class)}
+)
 public class RootConfig
 {
 }
--- src/main/java/app/config/WebConfig.java
+++ src/main/java/app/config/WebConfig.java
@@ -1,8 +1,16 @@
 package app.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 /**
  * @author ngnmhieu
  */
-public class WebConfig
+@Configuration
+@ComponentScan(basePackages = "app.web") // search for beans in package app.web
+@EnableWebMvc // use spring-mvc default configurations
+public class WebConfig extends WebMvcConfigurerAdapter
 {
 }
