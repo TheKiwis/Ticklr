20c513bd860d21e25ec7a685b1808a6045da9dc4
ngnmhieu <ngnmhieu@gmail.com>
2016-02-27 16:45:10 +0100

    add dbunit, write an integration test

--- src/main/java/app/web/UserController.java
+++ src/main/java/app/web/UserController.java
@@ -22,7 +22,6 @@ import java.util.Map;
 /**
  * @author ngnmhieu
  */
-//@RestController
 @RestController
 @RequestMapping("/users")
 public class UserController
--- src/test/java/app/integration/UserManipulationIT.java
+++ src/test/java/app/integration/UserManipulationIT.java
@@ -5,13 +5,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import app.config.RootConfig;
-import app.data.UserRepository;
+import app.config.WebConfig;
+import org.dbunit.DBTestCase;
+import org.dbunit.DataSourceBasedDBTestCase;
+import org.dbunit.assertion.DbUnitAssert;
+import org.dbunit.dataset.DefaultDataSet;
+import org.dbunit.dataset.IDataSet;
 import org.flywaydb.test.annotation.FlywayTest;
 import org.flywaydb.test.junit.FlywayTestExecutionListener;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
@@ -23,21 +27,33 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.sql.DataSource;
+import javax.validation.groups.Default;
+import javax.xml.crypto.Data;
+
 /**
  * @author ngnmhieu
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = RootConfig.class)
-@WebAppConfiguration
-@TestExecutionListeners({DependencyInjectionTestExecutionListener.class,
-        FlywayTestExecutionListener.class})
+@ContextConfiguration(classes = {RootConfig.class, WebConfig.class})
+@WebAppConfiguration("src/main/java/")
+@TestExecutionListeners({
+        DependencyInjectionTestExecutionListener.class,
+        FlywayTestExecutionListener.class
+})
 @ActiveProfiles("test")
 @FlywayTest
-public class UserManipulationIT
+public class UserManipulationIT extends DataSourceBasedDBTestCase
 {
-    // todo integration test with database fixture
     @Autowired
-    UserRepository userRepository;
+    DataSource dataSource;
+
+    // todo integration test with database fixture
+    @PersistenceContext
+    EntityManager em;
 
     @Autowired
     WebApplicationContext wac;
@@ -53,12 +69,32 @@ public class UserManipulationIT
     @Test
     public void testSaveUser() throws Exception
     {
+        Query query;
+
+        query = em.createQuery("SELECT COUNT(u) FROM User u");
+        assertEquals(0l, query.getSingleResult());
+
         mockMvc.perform(
                 post("/users/")
+                        .accept("application/json")
                         .param("email", "email@gmail.com")
                         .param("password", "123456789"))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$").isEmpty());
+
+        query = em.createQuery("SELECT COUNT(u) FROM User u");
+        assertEquals(1l, query.getSingleResult());
     }
 
+    @Override
+    protected DataSource getDataSource()
+    {
+        return dataSource;
+    }
+
+    @Override
+    protected IDataSet getDataSet() throws Exception
+    {
+        return new DefaultDataSet();
+    }
 }
